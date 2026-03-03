f2f66331afc7620747d900c5ef01ae33355fd89f
Narendra <ngali8032@gmail.com>
2026-03-03 18:16:29 +0530

    Update client.c

--- client.c
+++ client.c
@@ -1,11 +1,4 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <netinet/ip.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include "header.h"
 
 int main(int argc, char *argv[])
 {
